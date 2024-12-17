bcf695c27b18185cfa3d321894af05a72fc751bf
Lindo <mlambolindo6@gmail.com>
2024-12-17 19:22:06 +0200

    - Added validation for the password input

--- main.js
+++ main.js
@@ -3,30 +3,67 @@ const $ = (q, selectAll = true) =>
 
 window.onload = () => {
   console.log("ready...");
+
   const bitCheckboxes = $("input.bit-inp:not(:last-child)");
   const confirmInput = $("input.bit-inp:last-child", false);
+  const hintText = $("div.hint-text", false);
   const passwordInput = $("#password-inp", false);
   const form = $("form", false);
-
-  console.log(confirmInput);
+  let hintMessage = { message: "", isError: false };
 
   confirmInput.onclick = () => {
     const binaryString = [...bitCheckboxes]
       .map((c) => (c.checked ? "1" : "0"))
       .join("");
 
-    if (binaryString === "0001000") {
-      passwordInput.value = passwordInput.value.substr(
-        0,
-        passwordInput.value.length - 1
-      );
+    if (isBackspaceKey(binaryString)) {
+      passwordInput.value = removeLastCharacter(passwordInput.value);
+      hintMessage = getPasswordValidationResult(passwordInput.value);
+    } else if (isEnterKey(binaryString)) {
+      if (isPasswordCorrectLength(passwordInput.value)) {
+        hintMessage.message = "Your password has been succesfully chosen";
+      }
     } else {
-      const result = binaryToString(binaryString);
-      passwordInput.value += result;
+      passwordInput.value += binaryToString(binaryString);
+      hintMessage = getPasswordValidationResult(passwordInput.value);
     }
+
+    updateHintText(hintText, hintMessage);
 
     setTimeout(() => form.reset(), 100);
   };
+};
+
+const updateHintText = (hintTextElement, hintMessage) => {
+  if (hintMessage.isError) {
+    hintTextElement.classList.add("error");
+  } else {
+    hintTextElement.classList.remove("error");
+  }
+
+  hintTextElement.innerHTML = hintMessage.message;
+};
+
+const removeLastCharacter = (inputString) => {
+  return inputString.substr(0, inputString.length - 1);
+};
+
+const isBackspaceKey = (inputString) => inputString === "0001000";
+const isEnterKey = (inputString) => inputString === "0001101";
+
+const getPasswordValidationResult = (passwordString) => {
+  if (isPasswordCorrectLength(passwordString)) {
+    return { isError: false, message: "" };
+  }
+
+  return {
+    isError: true,
+    message: "Password must be 5 or more characters long",
+  };
+};
+
+const isPasswordCorrectLength = (passwordString) => {
+  return passwordString.length >= 5;
 };
 
 const binaryToString = (binaryString) => {
